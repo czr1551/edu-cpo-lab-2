e76c49e58d17c5d832fab98c42534fb3c9c1377a
czr1551 <czr1551@gmail.com>
2025-03-12 18:17:56 +0300

    Actions check

--- open_addressing_set.py
+++ open_addressing_set.py
@@ -9,8 +9,7 @@ class ImmutableOpenAddressingSet:
 
     EMPTY = object()
 
-    def __init__(self, initial_capacity=8, growth_factor=2,
-                 buckets=None, size=0):
+    def __init__(self, initial_capacity=8, growth_factor=2, buckets=None, size=0):
         """
         Initializes an immutable hash set.
 
@@ -41,8 +40,7 @@ class ImmutableOpenAddressingSet:
         """
         new_buckets = self.buckets.copy()
         return ImmutableOpenAddressingSet(
-            self.capacity, self.growth_factor,
-            buckets=new_buckets, size=self.size
+            self.capacity, self.growth_factor, buckets=new_buckets, size=self.size
         )
 
     def _hash(self, key):
@@ -57,8 +55,7 @@ class ImmutableOpenAddressingSet:
         the key in the bucket or the first empty slot.
         """
         index = self._hash(key)
-        while (self.buckets[index] is not self.EMPTY
-               and self.buckets[index] != key):
+        while self.buckets[index] is not self.EMPTY and self.buckets[index] != key:
             index = (index + 1) % self.capacity
         return index
 
@@ -109,8 +106,7 @@ class ImmutableOpenAddressingSet:
                 new_buckets[index] = key
         new_size = sum(1 for key in new_buckets if key is not self.EMPTY)
         return ImmutableOpenAddressingSet(
-            new_capacity, self.growth_factor,
-            buckets=new_buckets, size=new_size
+            new_capacity, self.growth_factor, buckets=new_buckets, size=new_size
         )
 
     def filter(self, predicate):
